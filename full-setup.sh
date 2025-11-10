78a66c109784a8cbfdf8d1cc7ad6f99a790d19d0
Paul Seica <pseica01@DESKTOP-C837STU.localdomain>
2025-11-10 18:49:20 +0200

    Added setup script

---
+++ full-setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+sudo apt-get install -y libglib2.0-dev libgcrypt20-dev zlib1g-dev autoconf automake libtool flex libpixman-1-dev bc make ninja-build libncurses-dev libelf-dev libssl-dev libcap-ng-dev libattr1-dev libslirp-dev libslirp0 git fakeroot build-essential ncurses-dev xz-utils libssl-dev bc libelf-dev bison python3-venv python3-pip
+pip install tomli
+mkdir cxl-qemu
+cd cxl-qemu
+git clone https://gitlab.com/qemu-project/qemu.git
+cd qemu
+git checkout stable-10.0
+./configure --target-list=x86_64-softmmu --enable-debug
+make -j$(nproc)
+cd ../
+git clone https://github.com/weiny2/linux-kernel.git
+cd linux-kernel
+git checkout dcd-v6-2025-09-23
+make defconfig
+rm .config
+mv ../.config .config
+make -j$(nproc)  
+cd ../  
+wget https://releases.ubuntu.com/plucky/ubuntu-25.04-live-server-amd64.iso  
+qemu/build/qemu-img create -f qcow2 ubuntu25.04.qcow2 30G
+
