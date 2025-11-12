b4abd8593b4829e9192ef0e4b31db40e0c9a6b7a
Paul Seica <pseica01@DESKTOP-C837STU.localdomain>
2025-11-12 17:33:30 +0200

    another error in full-setup

--- full-setup.sh
+++ full-setup.sh
@@ -19,7 +19,7 @@ rm .config
 mv ../../.config .config
 make -j$(nproc)
 sudo make modules_install
-sudo mkinitramfs -o initrd.img 6.17.0-rc+
+sudo mkinitramfs -o ../initrd.img 6.17.0-rc+
 cd ../  
 wget https://releases.ubuntu.com/plucky/ubuntu-25.04-live-server-amd64.iso  
 qemu/build/qemu-img create -f qcow2 ubuntu25.04.qcow2 30G
